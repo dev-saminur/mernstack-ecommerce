2a5b95e31725f3a9291d75c012a9c8b1b36746c8
dev-saminur <saminur9125@gmail.com>
2024-04-29 21:43:52 +0600

    Update registrationController.js

--- backend/controller/registrationController.js
+++ backend/controller/registrationController.js
@@ -1,4 +1,55 @@
-let registrationController = (req, res) => {
-  res.send("This is route form auth and registration controllers");
+const User = require("../model/userModel");
+const nodemailer = require("nodemailer");
+const bcrypt = require("bcrypt");
+const otpGenerator = require("otp-generator");
+
+let registrationController = async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.send({ error: "Please fill the full form" });
+  }
+  if (password && password.length < 6) {
+    return res.send({ error: "Password is too small" });
+  }
+
+  let excistingUser = await User.find({ email: email });
+  console.log(excistingUser);
+  if (excistingUser.length > 0) {
+    return res.send({ error: `${email} already in use` });
+  } else {
+    const otp = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      specialChars: false,
+    });
+    console.log(otp);
+    bcrypt.hash(password, 10, async function (err, hash) {
+      let user = new User({
+        name: name,
+        email: email,
+        password: hash,
+      });
+      user.save();
+
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "saminur9125@gmail.com",
+          pass: "iglr jkzb rrlr vapt ",
+        },
+      });
+      const info = await transporter.sendMail({
+        from: ` "MERNIAN"`, // sender address
+        to: email, // list of receivers
+        subject: "This is your OTP", // Subject line
+        text: "Hello world?", // plain text body
+        html: `Here is your <b> OTP:</b> ${otp}`, // html body
+      });
+      res.send({
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      });
+    });
+  }
 };
 module.exports = registrationController;
