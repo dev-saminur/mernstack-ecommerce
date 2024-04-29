66192a144f6e2c96c99fd42677ef2ced0e6b8859
dev-saminur <saminur9125@gmail.com>
2024-04-29 21:44:08 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const route = require("./routes");
 const express = require("express");
 const app = express();
 
+app.use(express.json());
 mongoConfig();
 app.use("/", route);
 const port = process.env.PORT || 8000;
