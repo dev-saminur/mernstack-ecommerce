f4228d8e3d2185dc1ca27caeb51b79189b0d7922
dev-saminur <saminur9125@gmail.com>
2024-04-30 19:47:51 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
-const mongoConfig = require("./config/mongoConfig");
-const route = require("./routes");
 const express = require("express");
-const app = express();
+const route = require("./routes");
+const cors = require("cors");
 
-app.use(express.json());
+const app = express();
+const mongoConfig = require("./config/mongoConfig");
+
 mongoConfig();
+app.use(cors());
+app.use(express.json());
 app.use("/", route);
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log("Port 8000 connected");
