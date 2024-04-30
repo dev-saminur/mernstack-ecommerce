5de3c0108dc803479c24fcaac69a461405ba8684
dev-saminur <saminur9125@gmail.com>
2024-04-30 19:47:58 +0600

    Update secureApi.js

--- backend/middleware/secureApi.js
+++ backend/middleware/secureApi.js
@@ -2,6 +2,7 @@ let secureApi = (req, res, next) => {
   if (req.headers.authorization == "saminur1234") {
     next();
   } else {
+    res.status(401);
     res.send({ error: "Inviled APi" });
   }
 };
