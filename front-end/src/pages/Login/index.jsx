a4172fadb8750de2e03dca85a81c686ba0cd7fd3
dev-saminur <saminur9125@gmail.com>
2024-05-10 11:26:24 +0600

    loginpage

---
+++ front-end/src/pages/Login/index.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { Button, Alert, Space, Input, Form } from "antd";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const [msg, setMsg] = useState("");
+  const navigate = useNavigate();
+
+  const onFinish = async (values) => {
+    console.log("Success:", values);
+    setLoading(true);
+
+    let data = await axios.post("http://localhost:8000/api/v1/auth/login", {
+      email: values.email,
+      password: values.password,
+    });
+
+    setMsg(data.data.success);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <>
+      <div>
+        {msg && <Alert message={msg} type="success" showIcon closable />}
+
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your email!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button
+              type="primary"
+              htmlType="submit"
+              loading={loading}
+              disabled={loading}
+            >
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
