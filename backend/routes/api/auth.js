eb592059e9deeabcaae185f9c250222afac387db
dev-saminur <saminur9125@gmail.com>
2024-05-10 11:24:53 +0600

    Update auth.js

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const registrationController = require("../../controller/registrationController");
 const secureApi = require("../../middleware/secureApi");
+const otpController = require("../../controller/otpController");
+const loginController = require("../../controller/loginController");
+const linkController = require("../../controller/linkController");
 const route = express.Router();
 
 const app = express();
 
-route.post("/registration", secureApi, registrationController);
+// route.post("/registration", secureApi, registrationController);  // Middle ware rakhte chaile  registration er pore secureApi use korbo.
+route.post("/registration", registrationController);
+route.post("/login", loginController);
+route.post("/otpvarification", otpController);
+route.post("/linkvarification", linkController);
+
 module.exports = route;
