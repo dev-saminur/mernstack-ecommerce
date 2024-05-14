daad1887e1495c7e6a442782c1ffc78cc4a22c71
dev-saminur <saminur9125@gmail.com>
2024-05-14 11:04:04 +0600

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -11,126 +11,31 @@ import Registration from "./pages/Registration";
 import OtpVarification from "./pages/OtpVarification";
 import Login from "./pages/Login";
 import EmailVerifyLink from "./pages/EmailVerifyLink";
+import ForgotPass from "./pages/ForgotPass/ForgotPass";
+import NewPass from "./pages/NewPass/NewPass";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Adcategory from "./pages/Adcategory/Adcategory";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<Registration />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/forgotpass" element={<ForgotPass />} />
+      <Route path="/newpassword/:token" element={<NewPass />} />
       <Route path="/emailverification/:token" element={<EmailVerifyLink />} />
       <Route path="/otpvarification/:email" element={<OtpVarification />} />
+      <Route path="/dashboard" element={<Dashboard />}>
+        <Route path="addcategory" element={<Adcategory />} />
+      </Route>
     </Route>
   )
 );
 
 function App() {
-  // const [loading, setLoading] = useState(false);
-  // const [msg, setMsg] = useState("");
-  // const onFinish = async (values) => {
-  //   console.log("Success:", values);
-  //   setLoading(true);
-
-  //   let data = await axios.post(
-  //     "http://localhost:8000/api/v1/auth/registration",
-  //     {
-  //       name: values.username,
-  //       email: values.email,
-  //       password: values.password,
-  //     },
-  //     {
-  //       headers: {
-  //         Authorization: "saminur1234",
-  //       },
-  //     }
-  //   );
-  //   console.log(data);
-  //   setLoading(false);
-  //   setMsg("Registration Successfull. Please check your email");
-  // };
-
-  // const onFinishFailed = (errorInfo) => {
-  //   console.log("Failed:", errorInfo);
-  // };
-
   return (
     <>
       <RouterProvider router={router} />
-      {/* <div>
-        {msg && <Alert message={msg} type="success" showIcon closable />}
-
-        <Form
-          name="basic"
-          labelCol={{
-            span: 8,
-          }}
-          wrapperCol={{
-            span: 16,
-          }}
-          style={{
-            maxWidth: 600,
-          }}
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[
-              {
-                required: true,
-                message: "Please input your username!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: "Please input your email!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: "Please input your password!",
-              },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-
-          <Form.Item
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
-            <Button
-              type="primary"
-              htmlType="submit"
-              loading={loading}
-              disabled={loading}
-            >
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </div> */}
     </>
   );
 }
