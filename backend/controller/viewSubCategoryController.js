79a53dfea6894ee32cff81922d3ac31d57e65112
dev-saminur <saminur9125@gmail.com>
2024-05-14 13:20:44 +0600

    Update viewSubCategoryController.js

--- backend/controller/viewSubCategoryController.js
+++ backend/controller/viewSubCategoryController.js
@@ -1,8 +1,9 @@
 const SubCategory = require("../model/subCatrgoryModel");
+const Category = require("../model/categoryModel");
 
 let viewSubCategoryController = async (req, res) => {
-  let data = await SubCategory.find();
-  console.log(data);
+  let data = await SubCategory.find().populate("categoryId");
+  res.send(data);
 };
 
 module.exports = viewSubCategoryController;
